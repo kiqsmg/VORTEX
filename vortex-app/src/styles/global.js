e046d38372ef386c11eadb371a805f94117c6718
kiqsmg <kiqsmg@gmail.com>
2023-09-28 14:55:09 -0300

    Create vortex project inicialization

---
+++ vortex-app/src/styles/global.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.COLORS.BACKGRUND_main};
+        color: ${({ theme }) => theme.COLORS.WHITE};
+
+        -webkit-font-smoothing: antialiased;
+    }
+
+    border-style, :-ms-input-placeholder, :no-button, textarea {
+        font-family: 'Poppins', serif;  //Não sei se vai dar certo
+        font-size: 12px;
+        outline: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
+    }
+`
---
+++ vortex-app/src/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+    COLORS: {
+        BACKGROUND_main: "linear-gradient(0deg, #0F144C, #0F144C)",
+        BACKGROUND_header: "rgba(24, 42, 105, 1)",
+        BACKGROUND_lighter: "rgba(24, 42, 105, 1)",
+
+        BACKGROUND_grey: "rgba(185, 185, 185, 1)",
+        BACKGROUND_greyish: "linear-gradient(179.86deg, #E5E5E5 53.44%, #A2A2A2 99.88%)",
+
+        BACKGROUND_light_blue: "rgba(0, 240, 255, 1)",
+        BACKGROUND_dark_blue: "rgba(14, 20, 76, 1)",
+
+        BACKGROUND_assets: "rgba(88, 109, 186, 1)",
+        BACKGROUND_team: "rgba(51, 87, 120, 1)",
+
+
+        WHITE: "rgba(249, 249, 249, 1)",
+        PURPLE: "rgba(125, 47, 224, 1)",
+        OCEAN_GREEN: "rgba(23, 239, 187, 1)",
+        RED: "rgba(224, 47, 47, 1)",
+    }
+};
