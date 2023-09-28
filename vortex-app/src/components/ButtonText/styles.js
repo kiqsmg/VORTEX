77d962542bfdf6be1a90bd2c443c2e750caafef3
kiqsmg <kiqsmg@gmail.com>
2023-09-28 16:58:36 -0300

    Create ButtonText index and style, also fix ButtonRound style bug

---
+++ vortex-app/src/components/ButtonText/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    background: none;
+
+    color: ${({ theme }) => theme.COLORS.PURPLE};
+
+    border: none;
+    font-size: 12px;
+`
