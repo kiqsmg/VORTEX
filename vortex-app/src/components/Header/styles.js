6b2b1c71dfb04b96cebe1ab2197c8f2f89f048da
kiqsmg <kiqsmg@gmail.com>
2023-10-05 11:14:39 -0300

    Add Header Components and style

---
+++ vortex-app/src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Link} from 'react-router-dom';
+
+export const Container = styled.header`
+    grid-area: header;  //mantem header fixo
+
+    height: 57px;
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+
+    padding: 6px 10px;
+`;
+
