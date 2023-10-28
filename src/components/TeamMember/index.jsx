633fcff167758dcca4f93189f5d099ad6ee12189
kiqsmg <kiqsmg@gmail.com>
2023-10-27 21:25:24 -0300

    Create TeamMember component

---
+++ src/components/TeamMember/index.jsx
@@ -0,0 +1,22 @@
+import { Container } from './styles';
+
+export function TeamMember() {
+    return (
+        <Container>
+            <div className='bot'>
+
+                <img src="" alt="" />
+                
+                <h3>Luffy Muguiara</h3>
+                <h4>Sofware Developer</h4>
+
+                <li><a href="https://twitter.com/BarackObama" target="_blank" id="Twitter"></a></li>
+                <li><a href="https://twitter.com/BarackObama" target="_blank" id="Twitter"></a></li>
+                <li><a href="https://twitter.com/BarackObama" target="_blank" id="Twitter"></a></li>
+
+
+
+            </div>
+        </Container>
+    );
+}
